8b0e0e2e7675c7aed987e29a5d3931f0887f2f77
ddthj <mwirsing@rthighschool.org>
2017-11-08 13:56:57 -0500

    ColTestStuff
    workin on it

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ square_offsets=[(100,315),
                 (100,45),
                 (100,135),
                 (100,225)]
+def s(scale):
+    return [(scale+4,315),
+            (scale+4,45),
+            (scale+4,135),
+            (scale+4,225)]
+    
 def r(scale):
     sides = random.randint(3,10)
     c = int(360/sides)
@@ -83,7 +89,7 @@ class gobject():
         self.color = [a,b,c]
         self.vx = random.randint(-25,25)
         self.vy = random.randint(-25,25)
-        self.vr = random.randint(-14,14)
+        self.vr = 0#random.randint(-14,14)
         self.rotation = 0
         self.centerx = center[0]
         self.centery = center[1]
@@ -110,7 +116,8 @@ class poly(gobject):
     def test(self):
         if self.f:
             self.f = False
-            return me(self.color,self.centerx,self.centery,self.rotation,self.offsets)
+            return False
+            #return me(self.color,self.centerx,self.centery,self.rotation,self.offsets)
         else:
             return False
     def render(self,scale):
@@ -143,60 +150,55 @@ def get_inside(point,obj):
     cords = obj.convert_offsets()
     counts = 0
     for x in range(len(cords)-1):
-        if x+1 < len(cords):
+        if x+1 <= len(cords):
             a = cords[x]
             b = cords[x+1]
         else:
             a = cords[x]
             b = cords[0]
 
-        if a[1] > point[1] > b[1] or a[1] < point[1] < b[1]:
-            counts += 1
+        if a[1] >= point[1] >= b[1] or a[1] <= point[1] <= b[1]:
+            if a[0] >= point[0] and b[0] >= point[0]:
+                counts += 1
     if counts % 2 > 0:
         return True
     return False
 
 def tick(objects,scale):
     #move objects
-    for item in objects:
-        item.centerx += item.vx
-        item.centery += item.vy
-        item.rotation += item.vr
-        
-        item.render(scale)
-    #check for pairs
-    warning = []
-    '''
     for a in objects:
+        a.centerx += a.vx
+        a.centery += a.vy
+        a.rotation += a.vr
+        a.render(scale)
+        Flag = False
         for b in objects:
-            if a != b and d((a.centerx,a.centery),(b.centerx,b.centery)) <= a.danger+b.danger:
-                warning.append((a,b))
-    '''
-    for item in warning:
-        a = item[0]
-        b = item[1]
-        for item in a.convert_offsets():
-            if get_inside(item,b):
-                pass
-                #print("c")
-        for item in b.convert_offsets():
-            if get_inside(item,a):
-                pass
-                #print("c")
+            if a != b:
+                for i in a.convert_offsets():
+                    if get_inside(i,b):
+                        Flag = True
+                        break
+        if Flag:
+            a.vx *= -1
+            a.vy *= -1
+    return objects
 
 c = pygame.time.Clock()
 objs = []
-for x in range(50):
-    objs.append(poly((random.randint(0,screenx),random.randint(0,screeny)),r(scale)))
+for x in range(25):
+
+    objs.append(poly((random.randint(0,screenx),random.randint(0,screeny)),s(scale)))
+    objs.append(poly((random.randint(0,screenx),random.randint(0,screeny)),s(scale)))
+
 while 1:
     c.tick(20)
     window.fill(white)
-    tick(objs,scale)
+    objs = tick(objs,scale)
     new = objs
     objs = []
     for item in new:
         if item.centerx > screenx+100 or item.centerx < -100 or item.centery > screeny+100 or item.centery < -100:
-            objs.append(poly((random.randint(0,screenx),random.randint(0,screeny)),r(scale)))
+            objs.append(poly((random.randint(0,screenx),random.randint(0,screeny)),s(scale)))
         else:
             if item.dead == False:
                 x = item.test()
